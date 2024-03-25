5ca6656bc365cbceee38cac054a525f4bbf63486
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-25 10:46:53 +0100

    01 tutorial-javascript: Server Components vs Client Components

--- 01-nextjs-tutorial-javascript/app/client/page.jsx
+++ 01-nextjs-tutorial-javascript/app/client/page.jsx
@@ -1,3 +1,17 @@
-export default function AboutInfoPage() {
-  return <h1 className="text-7xl">Client Page</h1>;
+"use client";
+
+import { useState } from "react";
+
+export default function ClientPage() {
+  const [count, setCount] = useState(0);
+
+  console.log("visible on client side");
+  return (
+    <div>
+      <h1 className="text-7xl font-bold mb-4 ">{count}</h1>
+      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+        increase
+      </button>
+    </div>
+  );
 }
--- 01-nextjs-tutorial-javascript/app/page.jsx
+++ 01-nextjs-tutorial-javascript/app/page.jsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
 export default function Home() {
+  console.log("visible on server side");
   return (
     <div>
-      <h1 className="text-7xl">HomePage</h1>
-      <Link href="/about" className="text-2xl">
-        about page
+      <h1 className="text-5xl mb-8 font-bold">Next.js Tutorial</h1>
+      <Link href="/client" className="btn btn-accent">
+        get started
       </Link>
     </div>
   );
