4e16876ed84d45ab21b7ede602e66702f9b8d6fd
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-25 06:28:02 +0100

    Intro +  New App + Home Page + Create Pages & Link  + Component + Nested Pages + First Challenge

---
+++ 01-nextjs-tutorial-javascript/app/about/page.jsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-7xl">AboutPage</h1>
+    </div>
+  );
+}
---
+++ 01-nextjs-tutorial-javascript/app/tasks/page.jsx
@@ -0,0 +1,3 @@
+export default function TaskPage() {
+  return <h1 className="text-7xl">AboutInfoPage</h1>;
+}
