645b778d53a30871262246b3fe61eebeec73f67f
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-29 12:34:05 +0100

    01 tutorial-typescript: fetch data: cocktaildb API

--- 01-nextjs-tutorial-typescript/app/drinks/page.tsx
+++ 01-nextjs-tutorial-typescript/app/drinks/page.tsx
@@ -1,3 +1,14 @@
-export default function DrinkPage() {
-  return <h1 className="text-7xl"> Drinks Page</h1>;
+/** NOTE: data type is not handled her for now, TODO in few time */
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+export default async function DrinkPage() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return (
+    <div>
+      <h1 className="text-7xl">DrinksPage</h1>
+    </div>
+  );
 }
