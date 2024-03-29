ca451b20303e25ec951e0d74e266a9a283f4ae3a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-29 12:33:55 +0100

    01 tutorial-javascript: fetch data: cocktaildb API

--- 01-nextjs-tutorial-javascript/app/drinks/page.jsx
+++ 01-nextjs-tutorial-javascript/app/drinks/page.jsx
@@ -1,3 +1,12 @@
-export default function DrinkPage() {
-  return <h1 className="text-7xl">Drinks Page</h1>;
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+export default async function DrinkPage() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return (
+    <div>
+      <h1 className="text-7xl">DrinksPage</h1>
+    </div>
+  );
 }
