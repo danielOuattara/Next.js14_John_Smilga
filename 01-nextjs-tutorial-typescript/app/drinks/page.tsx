8ff49cbba064aec735499a8b99f7ecf1c97e0514
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-29 12:59:38 +0100

    01 tutorial-typescript: fetch data: cocktaildb API + next loading

--- 01-nextjs-tutorial-typescript/app/drinks/page.tsx
+++ 01-nextjs-tutorial-typescript/app/drinks/page.tsx
@@ -1,10 +1,32 @@
 /** NOTE: data type is not handled her for now, TODO in few time */
 
+// const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+// export default async function DrinkPage() {
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   console.log(data);
+//   return (
+//     <div>
+//       <h1 className="text-7xl">DrinksPage</h1>
+//     </div>
+//   );
+// }
+
+//-----------------------------------------------------------------------
+
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-export default async function DrinkPage() {
+const fetchDrinks = async () => {
+  // just for demo purposes
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(url);
   const data = await response.json();
+  return data;
+};
+
+export default async function DrinkPage() {
+  const data = await fetchDrinks();
   console.log(data);
   return (
     <div>
