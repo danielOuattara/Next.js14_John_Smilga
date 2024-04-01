a9c57918f0db6640df3b5c500b86b34deb816d1e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-01 20:32:32 +0200

    01 tutorial-TS: 34 first Server Action

--- 01-nextjs-tutorial-typescript/components/TaskForm.tsx
+++ 01-nextjs-tutorial-typescript/components/TaskForm.tsx
@@ -1,3 +1,36 @@
+import prisma from "@/utilities/db";
+import { revalidatePath } from "next/cache";
+
 export default function TaskForm() {
-  return <div>task form</div>;
+  return (
+    <form action={createTask}>
+      <div className="join w-full">
+        <input
+          className="input input-bordered join-item w-full"
+          placeholder="Type Here"
+          type="text"
+          name="content"
+          required
+        />
+        <button type="submit" className="btn join-item btn-primary">
+          create task
+        </button>
+      </div>
+    </form>
+  );
+}
+
+//------------------------------
+async function createTask(formaData: FormData) {
+  "use server";
+  const content = formaData.get("content") as string;
+
+  // some validation if necessary
+
+  await prisma.task.create({
+    data: { content },
+  });
+
+  // revalidate path
+  revalidatePath("/tasks");
 }
