a3a17ec93e82e0ab4a3f06ba6d13f092a53c5522
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-01 07:19:26 +0200

    01 tutorial-JS: Responsive Image

--- 01-nextjs-tutorial-javascript/app/layout.jsx
+++ 01-nextjs-tutorial-javascript/app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <nav className="px-8 py-20 max-w-6xl mx-auto">{children}</nav>
+        <div className="px-8 py-20 max-w-6xl mx-auto">{children}</div>
       </body>
     </html>
   );
