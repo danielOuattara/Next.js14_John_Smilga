367129963759066b02a5801306bccb0fcb9d2cf4
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-14 18:57:20 +0200

    02-gpt-genius-app-TS: 67: ThemeToggle comp. logic

--- 02-gpt-genuis-app-nextjs-typescript/src/components/ThemeToggle.tsx
+++ 02-gpt-genuis-app-nextjs-typescript/src/components/ThemeToggle.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import { useState } from "react";
+
+const themesList = ["winter", "dracula"];
+
 export default function ThemeToggle() {
-  return <button className="btn btn-primary btn-sm">ThemeToggle</button>;
+  const [theme, setTheme] = useState(themesList[0]);
+  const themeToggler = () => {
+    const themeToSet = theme === themesList[0] ? themesList[1] : themesList[0];
+    setTheme(() => themeToSet);
+    document.documentElement.setAttribute("data-theme", themeToSet);
+  };
+  return (
+    <button className="btn btn-outline btn-sm" onClick={() => themeToggler()}>
+      {theme === themesList[0] ? (
+        <BsMoonFill className="h-4 w-4" />
+      ) : (
+        <BsSunFill className="h-4 w-4" />
+      )}
+    </button>
+  );
 }
