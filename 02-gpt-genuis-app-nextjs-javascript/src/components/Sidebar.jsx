4004ceecbdac1b332b0a6d281015a606b0797408
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-14 15:37:43 +0200

    02-gpt-genius-app-JS: 63: Global Sidebar comp.

--- 02-gpt-genuis-app-nextjs-javascript/src/components/Sidebar.jsx
+++ 02-gpt-genuis-app-nextjs-javascript/src/components/Sidebar.jsx
@@ -1,3 +1,14 @@
+import { SidebarHeader, NavLinks, MemberProfile } from "./index";
+
 export default function Sidebar() {
-  return <div className="text-6xl">Sidebar</div>;
+  return (
+    <div className="px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto] ">
+      {/* first row */}
+      <SidebarHeader />
+      {/* second row */}
+      <NavLinks />
+      {/* third row */}
+      <MemberProfile />
+    </div>
+  );
 }
--- 02-gpt-genuis-app-nextjs-javascript/src/components/index.jsx
+++ 02-gpt-genuis-app-nextjs-javascript/src/components/index.jsx
@@ -1,1 +1,4 @@
+export { default as MemberProfile } from "./MemberProfile";
+export { default as NavLinks } from "./NavLinks";
 export { default as Sidebar } from "./Sidebar";
+export { default as SidebarHeader } from "./SidebarHeader";
