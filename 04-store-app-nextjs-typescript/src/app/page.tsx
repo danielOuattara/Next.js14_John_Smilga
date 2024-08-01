6382a62377264ee41fd30ccd35a634c06e30f5db
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-02 01:29:14 +0200

    04-store-app-nextjs-typescript: install all shdcn/ui components for the app

--- 04-store-app-nextjs-typescript/src/app/page.tsx
+++ 04-store-app-nextjs-typescript/src/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
-  return <h1 className="text-2xl">HomePage</h1>;
+  return (
+    <>
+      <h1 className="text-2xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </>
+  );
 }
