58111ad339ba201cd46a0d4c7512309a80582395
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-02 19:24:04 +0200

    04-store-app-nextjs-typescript: clear TestProfile model + add Product model

--- 04-comfy-store-app-nextjs-typescript/src/app/about/page.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/app/about/page.tsx
@@ -1,17 +1,3 @@
-import prisma from "@/utils/prisma";
-
-export default async function AboutPage() {
-  await prisma.testProfile.create({
-    data: { name: "John Doe Junior" },
-  });
-
-  const users = await prisma.testProfile.findMany();
-  return (
-    <>
-      <h2>User List</h2>
-      {users.map((user) => (
-        <p key={user.id}>{user.name}</p>
-      ))}
-    </>
-  );
+export default function AboutPage() {
+  return <h2>AboutPage</h2>;
 }
