34b0e35f830885b4dd3c588f98d2f540df2468d0
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-02 02:19:29 +0200

    04-store-app-nextjs-typescript: NavSearch & CartButton Components

--- 04-store-app-nextjs-typescript/src/components/navbar/CartButton.tsx
+++ 04-store-app-nextjs-typescript/src/components/navbar/CartButton.tsx
@@ -1,3 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { LuShoppingCart } from "react-icons/lu";
+import Link from "next/link";
+
 export default function CartButton() {
-  return <div>CartButton</div>;
+  // temporary hard coded
+  const numItemsInCart = 9;
+
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link href="/cart">
+        <LuShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 }
