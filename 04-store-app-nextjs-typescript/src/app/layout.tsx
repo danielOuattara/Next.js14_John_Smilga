f89e5576f5fa0ead4ae610b448f7ba147ceda2f3
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-02 02:04:21 +0200

    04-store-app-nextjs-typescript: Navbar Component  Setup blueprint

--- 04-store-app-nextjs-typescript/src/app/layout.tsx
+++ 04-store-app-nextjs-typescript/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/navbar";
+import { Container } from "@/components/global";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <Container className="border rounded border-fuchsia-400 my-2 py-20">
+          {children}
+        </Container>
+      </body>
     </html>
   );
 }
--- 04-store-app-nextjs-typescript/src/components/navbar/Navbar.tsx
+++ 04-store-app-nextjs-typescript/src/components/navbar/Navbar.tsx
@@ -1,3 +1,16 @@
+import { CartButton, DarkMode, LinksDropdown, Logo, NavSearch } from "./index";
+import { Container } from "../global/index";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Container className="flex flex-col flex-wrap gap-4 py-8 sm:flex-row sm:justify-between sm:items-center border border-lime-800 rounded">
+      <Logo />
+      <NavSearch />
+      <div className="flex gap-4 items-center border border-lime-800 rounded">
+        <CartButton />
+        <DarkMode />
+        <LinksDropdown />
+      </div>
+    </Container>
+  );
 }
