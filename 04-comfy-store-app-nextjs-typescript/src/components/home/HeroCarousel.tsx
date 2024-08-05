28bff3e816f4f127c408a89181e382e412418891
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-05 18:15:06 +0200

    04-store-app-nextjs-typescript: HeroCarousel comp. OK

--- 04-comfy-store-app-nextjs-typescript/src/components/home/HeroCarousel.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/components/home/HeroCarousel.tsx
@@ -1,3 +1,37 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { carouselImages } from "@/utils/carouselImages";
+
 export default function HeroCarousel() {
-  return <h2>HeroCarousel</h2>;
+  return (
+    <section>
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, index) => (
+            <CarouselItem key={index}>
+              <Card>
+                <CardContent className="p-2">
+                  <Image
+                    src={image}
+                    alt="hero"
+                    className="w-full h-[24rem] rounded-md object-cover"
+                    loading="lazy"
+                  />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </section>
+  );
 }
---
+++ 04-comfy-store-app-nextjs-typescript/src/utils/carouselImages.ts
@@ -0,0 +1,9 @@
+import hero1 from "@/public/images/hero_1.jpg";
+import hero2 from "@/public/images/hero_2.jpg";
+import hero3 from "@/public/images/hero_3.jpg";
+import hero4 from "@/public/images/hero_4.jpg";
+import hero5 from "@/public/images/hero_5.jpg";
+import hero6 from "@/public/images/hero_6.jpg";
+import hero7 from "@/public/images/hero_7.jpg";
+
+export const carouselImages = [hero1, hero2, hero3, hero4, hero5, hero6, hero7];
