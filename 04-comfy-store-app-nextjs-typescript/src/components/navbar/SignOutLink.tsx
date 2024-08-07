6caabd9b8bf1d547e1cd109ca6619e3a75977ddc
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-07 14:29:58 +0200

    04-store-app-nextjs-typescript: Sign out logic & UserIcon comp.

--- 04-comfy-store-app-nextjs-typescript/src/components/navbar/SignOutLink.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/components/navbar/SignOutLink.tsx
@@ -1,3 +1,18 @@
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { useToast } from "../ui/use-toast";
+
 export default function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+  function handleLogout(): void {
+    toast({ description: "Logging Out..." });
+  }
+
+  return (
+    <SignOutButton>
+      <Link href="/" className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 }
--- 04-comfy-store-app-nextjs-typescript/src/components/navbar/UserIcon.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/components/navbar/UserIcon.tsx
@@ -1,3 +1,18 @@
-export default function UserIcon() {
-  return <div>UserIcon</div>;
+import { LuUser2 } from "react-icons/lu";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+
+export default async function UserIcon() {
+  const user = await currentUser();
+  if (user?.imageUrl) {
+    return (
+      <Image
+        src={user.imageUrl}
+        className="w-6 h-6 rounded-full object-cover"
+        alt={user.firstName || "user"}
+      />
+    );
+  } else {
+    return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
+  }
 }
