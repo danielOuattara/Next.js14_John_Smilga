763f8390517437a250ce52d5c8f8979cc0ca2f64
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-07 20:05:57 +0200

    04-store-app-nextjs-typescript:  custom  Button animated on action component.

---
+++ 04-comfy-store-app-nextjs-typescript/src/components/form/Buttons.tsx
@@ -0,0 +1,40 @@
+import { useFormStatus } from "react-dom";
+import { cn } from "@/lib/cn";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { LuTrash2, LuPenSquare } from "react-icons/lu";
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+
+type btnSize = "default" | "lg" | "sm";
+
+type TypeProps = {
+  className?: string;
+  text?: string;
+  size?: btnSize;
+};
+
+export default function Buttons({
+  className = "",
+  text = "submit",
+  size = "lg",
+}: TypeProps) {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      disabled={pending}
+      className={cn("capitalize", className)}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+}
