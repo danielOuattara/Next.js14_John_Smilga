041e4ea6c273caf44ef4643534b0d249d3422bfd
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-07 12:51:51 +0200

    04-store-app-nextjs-typescript: update FeaturedProducts

--- 04-comfy-store-app-nextjs-typescript/src/components/home/FeaturedProducts.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/components/home/FeaturedProducts.tsx
@@ -1,18 +1,12 @@
-import { fetchFeaturedProducts } from "@/utils/actions";
-import { EmptyList, LoadingContainer, SectionTitle } from "../global";
+import { LoadingProductsContainer, SectionTitle } from "../global";
 import { Suspense } from "react";
 import FeaturedProductsLoader from "./FeaturedProductsLoader";
 
 export default async function FeaturedProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  const products = await fetchFeaturedProducts();
-
-  if (products.length === 0) return <EmptyList />;
-
   return (
     <section className="pt-24">
       <SectionTitle text="featured products" />
-      <Suspense fallback={<LoadingContainer />}>
+      <Suspense fallback={<LoadingProductsContainer />}>
         <FeaturedProductsLoader />
       </Suspense>
     </section>
