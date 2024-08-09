efcc232a686e1bf62a45136eef5e73719c5ea174
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-09 23:55:39 +0200

    04-store-app-nextjs-typescript: custom Error message Zod validation: version 1

--- 04-comfy-store-app-nextjs-typescript/src/utils/actions.ts
+++ 04-comfy-store-app-nextjs-typescript/src/utils/actions.ts
@@ -4,7 +4,6 @@ import { currentUser } from "@clerk/nextjs/server";
 import prisma from "./prisma";
 import { redirect } from "next/navigation";
 import { productSchema } from "./zod-schemas";
-// import { productSchema } from "./valibot-schemas";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -63,40 +62,6 @@ export async function fetchSingleProduct(productId: string) {
   return product;
 }
 
-//----------
-
-// export const createProductAction = async (
-//   prevState: any,
-//   formData: FormData,
-// ) => {
-//   try {
-//     const user = await getAuthUser();
-//     //---
-//     const name = formData.get("name") as string;
-//     const company = formData.get("company") as string;
-//     const price = Number(formData.get("price"));
-//     const image = formData.get("image") as File;
-//     const description = formData.get("description") as string;
-//     const featured = Boolean(formData.get("featured") as string);
-
-//     await prisma.product.create({
-//       data: {
-//         name,
-//         company,
-//         price,
-//         image: "/images/product-1.jpg",
-//         description,
-//         featured,
-//         clerkId: user.id,
-//       },
-//     });
-
-//     return { message: "product created" };
-//   } catch (error) {
-//     return renderError(error);
-//   }
-// };
-
 // ---------
 // using Zod
 export const createProductAction = async (
@@ -107,7 +72,14 @@ export const createProductAction = async (
     const user = await getAuthUser();
     //---
     const rawData = Object.fromEntries(formData);
-    const validatedData = productSchema.parse(rawData);
+    const validatedFields = productSchema.safeParse(rawData);
+
+    if (!validatedFields.success) {
+      const errors = validatedFields.error.errors.map(
+        (error) => `\n - ${error.message}`,
+      );
+      throw new Error("Error:\n" + errors.join("\n"));
+    }
 
     // await prisma.product.create({
     //   data: {
@@ -133,6 +105,7 @@ export const createProductAction = async (
 // TODO: uncomment and try again validation on checkbox
 
 // import { parse } from "valibot";
+// import { productSchema } from "./valibot-schemas";
 
 // export const createProductAction = async (
 //   prevState: any,
