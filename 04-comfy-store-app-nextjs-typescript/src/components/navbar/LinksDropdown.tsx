951c4eaec66becb4e23fad0fccb6cfa289522844
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-15 14:50:26 +0200

    04-store-app-nextjs-typescript:  LinksDropdown comp. initial setup

--- 04-comfy-store-app-nextjs-typescript/src/components/navbar/LinksDropdown.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/components/navbar/LinksDropdown.tsx
@@ -1,3 +1,31 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LuAlignRight } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { links } from "@/utilities/links";
+
 export default function LinksDropdown() {
-  return <h4 className="text-xl">LinksDropdown Page</h4>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <LuAlignRight className="w-5 h-5" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="" align="end" sideOffset={10}>
+        {links.map((link) => (
+          <DropdownMenuItem key={link.href} className="flex justify-center">
+            <Link href={link.href} className="capitalize">
+              {link.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
