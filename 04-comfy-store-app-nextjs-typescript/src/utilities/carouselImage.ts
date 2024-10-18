8127f3dd0f15bf00ae4146fdb892b5c913a1bab0
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-19 00:10:00 +0200

    04-store-app-nextjs-typescript:  add  carouselImage in  utilities + install `embla-carousel` and its dependencies

---
+++ 04-comfy-store-app-nextjs-typescript/src/utilities/carouselImage.ts
@@ -0,0 +1,8 @@
+import wood1 from "@/../public/images/wood-1.jpg";
+import wood2 from "@/../public/images/wood-2.jpg";
+import wood3 from "@/../public/images/wood-3.jpg";
+import wood4 from "@/../public/images/wood-4.jpg";
+import wood5 from "@/../public/images/wood-5.jpg";
+import wood6 from "@/../public/images/wood-6.jpg";
+
+export const carouselImages = [wood3, wood1, wood2, wood4, wood5, wood6];
