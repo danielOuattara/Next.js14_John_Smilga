07ec56f89a0e4ea4d4ca0f49a40b659314d417bb
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-18 16:06:40 +0200

    04-store-app-nextjs-typescript:  fetch products function actions

---
+++ 04-comfy-store-app-nextjs-typescript/src/utilities/action.ts
@@ -0,0 +1,21 @@
+import prisma from "./prisma";
+
+//----------------
+
+export async function fetchFeaturedProducts() {
+  return await prisma.product.findMany({
+    where: {
+      featured: true,
+    },
+  });
+}
+
+//----------------
+
+export async function fetchAllProducts() {
+  return await prisma.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+}
