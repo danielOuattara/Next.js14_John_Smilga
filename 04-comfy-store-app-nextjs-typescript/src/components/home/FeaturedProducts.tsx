026afa643bd8a274ec06ce4b4652b68d9b1376a9
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-18 17:03:02 +0200

    04-store-app-nextjs-typescript:  FeaturedProducts comp.

--- 04-comfy-store-app-nextjs-typescript/src/components/home/FeaturedProducts.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/components/home/FeaturedProducts.tsx
@@ -1,3 +1,17 @@
-export default function FeaturedProducts() {
-  return <div>FeaturedProducts</div>;
+import { fetchFeaturedProducts } from "@/utilities/action";
+import { EmptyList, SectionTitle } from "../global";
+import { ProductsGrid } from "../products";
+
+export default async function FeaturedProducts() {
+  const products = await fetchFeaturedProducts();
+
+  if (products.length === 0) {
+    return <EmptyList />;
+  }
+  return (
+    <section>
+      <SectionTitle text="featured products" />
+      <ProductsGrid products={products} />
+    </section>
+  );
 }
