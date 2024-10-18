a9244fdbcf41bc99358eb1b9f1191ac01e638985
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-18 15:48:34 +0200

    04-store-app-nextjs-typescript:  render Hero & FeaturedProducts in home page

--- 04-comfy-store-app-nextjs-typescript/src/app/page.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { FeaturedProducts, Hero } from "@/components/home";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1 className="text-3xl">HomePage</h1>
-      <Button variant="outline" size="lg" className="capitalize m-8">
-        Click me
-      </Button>
-    </div>
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
   );
 }
