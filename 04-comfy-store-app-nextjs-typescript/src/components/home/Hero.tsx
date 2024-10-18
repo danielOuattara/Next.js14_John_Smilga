d577613b8f92541b79c20b72370833a6f0afbb85
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-19 00:12:19 +0200

    04-store-app-nextjs-typescript: HeroCarousel component

--- 04-comfy-store-app-nextjs-typescript/src/components/home/Hero.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/components/home/Hero.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { HeroCarousel } from "./index";
+
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-3 gap-16 items-center p-4">
+      <HeroCarousel />
+      <article className="lg:col-start-3 lg:col-end-auto">
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          Shop with us and live like never before
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et
+          voluptas saepe in quae voluptate, laborum maiores possimus illum.
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link href="/products">Shop now</Link>
+        </Button>
+      </article>
+    </section>
+  );
 }
--- 04-comfy-store-app-nextjs-typescript/src/components/home/HeroCarousel.tsx
+++ 04-comfy-store-app-nextjs-typescript/src/components/home/HeroCarousel.tsx
@@ -1,3 +1,45 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import { carouselImages } from "@/utilities/carouselImage";
+import Autoplay from "embla-carousel-autoplay";
+
 export default function HeroCarousel() {
-  return <div>HeroCarousel</div>;
+  return (
+    <div className="lg:block lg:col-start-1 lg:col-end-3">
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 3500,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {carouselImages.map((image) => (
+            <CarouselItem key={image.src}>
+              <Card>
+                <CardContent className="p-2">
+                  <Image
+                    src={image}
+                    alt="hero picture"
+                    className="w-full h-[24rem] rounded-md object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 }
