5a6df89f8d2dd6075a154431e4ca27019c1fa2bc
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-19 22:48:48 +0200

    03-jobify-app-nextjs-typescript: `Navbar` comp.

--- 03-jobify-app-nextjs-typescript/src/components/Navbar.tsx
+++ 03-jobify-app-nextjs-typescript/src/components/Navbar.tsx
@@ -1,3 +1,17 @@
+import LinksDropdown from "./LinksDropdown";
+import { UserButton } from "@clerk/nextjs";
+import { ThemeToggle } from "./index";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 flex items-center justify-between">
+      <div>
+        <LinksDropdown />
+      </div>
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton />
+      </div>
+    </nav>
+  );
 }
