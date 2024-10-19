ecfc8ea1cff85cf60f7e9209e66ee735b24dc08a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-19 20:49:17 +0200

    03-jobify-app-nextjs-typescript: added components: setup `layout`

--- 03-jobify-app-nextjs-typescript/src/app/(dashboard)/layout.tsx
+++ 03-jobify-app-nextjs-typescript/src/app/(dashboard)/layout.tsx
@@ -1,14 +1,17 @@
-type TypeProps = {
-  children: React.ReactNode;
-};
+import { Navbar, Sidebar } from "@/components";
+import { type PropsWithChildren } from "react";
 
-export default function DashboardLayout({ children }: TypeProps) {
+export default function DashboardLayout({ children }: PropsWithChildren) {
   return (
-    <div>
-      <>
-        <h1> dashboard layout</h1>
-        {children}
-      </>
-    </div>
+    <main className="grid lg:grid-cols-5">
+      <div className=" hidden lg:block lg:col-span-1 lg:min-h-screen">
+        <Sidebar />
+      </div>
+
+      <div className="lg:col-span-4">
+        <Navbar />
+        <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
+      </div>
+    </main>
   );
 }
