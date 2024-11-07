8eec7dcb561712f8f130a969f7220023b813dfca
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-07 11:26:30 +0100

    03-jobify-app-nextjs-typescript: basic shadcn/ui  Form & react-hook-form form design

--- 03-jobify-app-nextjs-typescript/src/app/(dashboard)/add-job/page.tsx
+++ 03-jobify-app-nextjs-typescript/src/app/(dashboard)/add-job/page.tsx
@@ -1,3 +1,9 @@
+import CreateJobForm from "@/components/form/CreateJobForm";
+
 export default function AddJobPage() {
-  return <div>add job page</div>;
+  return (
+    <>
+      <CreateJobForm />
+    </>
+  );
 }
