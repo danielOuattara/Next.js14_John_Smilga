4c3a1051c46db598a7e35bb8cc5450dc269da124
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-09 23:10:14 +0100

    03-jobify-app-nextjs-typescript: added navLinks

---
+++ 03-jobify-app-nextjs-typescript/src/utils/navLinks.tsx
@@ -0,0 +1,49 @@
+import { CopyPlus, ChartColumnIncreasing, SquareLibrary } from "lucide-react";
+
+type TypeNavLinks = {
+  href: string;
+  label: string;
+  icon: React.ReactNode;
+}[];
+
+const size = "36";
+// const color = "#F9A826";
+// const strokeWidth = 2;
+
+export const navLinks: TypeNavLinks = [
+  {
+    href: "/jobs",
+    label: "all jobs",
+    icon: (
+      <SquareLibrary
+        size={size}
+        className="text-primary"
+        // strokeWidth={strokeWidth}
+      />
+    ),
+  },
+  {
+    href: "/stats",
+    label: "view stats",
+    icon: (
+      <ChartColumnIncreasing
+        size={size}
+        // color={color}
+        className="text-primary"
+        // strokeWidth={strokeWidth}
+      />
+    ),
+  },
+  {
+    href: "/add-job",
+    label: "add a job",
+    icon: (
+      <CopyPlus
+        size={size}
+        className="text-primary"
+        // color={color}
+        // strokeWidth={strokeWidth}
+      />
+    ),
+  },
+];
