14c4c6eaef47b719b98602336b06ab2c748fb3cf
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-09 22:25:23 +0100

    03-jobify-app-nextjs-typescript: RootLayout & HomePage

--- 03-jobify-app-nextjs-typescript/src/app/page.tsx
+++ 03-jobify-app-nextjs-typescript/src/app/page.tsx
@@ -1,11 +1,41 @@
+import Image from "next/image";
+import logo from "./../assets/logo-2.svg";
+import landingImage from "./../assets/main-2.svg";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <Button variant={"outline"} size={"icon"} className="px-10 py-5">
-        click
-      </Button>
-    </div>
+    <main>
+      <header className="mx-auto px-10 md:px-20 py-6 border bg-blue-50">
+        <Image src={logo} alt="logo image" className="w-16 h-16" />
+      </header>
+      <section className="max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
+        <Image
+          src={landingImage}
+          alt="landing image showing a road to the top of a mountain in the background"
+          className=""
+        />
+        <div>
+          <h1 className="text-4xl md:text-6xl font-bold">
+            A <span className="capitalize text-primary">randonnée</span> for a
+            Job
+          </h1>
+          <p className="leading-loose max-w-md mt-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            ullam vero esse ratione nisi laudantium quo placeat nulla! Hic
+            laborum delectus veniam. Laborum nam modi consequatur, mollitia
+            ducimus similique unde, ipsum porro dolore nobis fuga harum beatae
+            alias dolor! Aspernatur atque repudiandae quam modi assumenda
+            dolorem accusantium corrupti? Quas, nihil.
+          </p>
+          <Button asChild>
+            <Link href={"/jobs"} className="m-8">
+              Let&apos;s go
+            </Link>
+          </Button>
+        </div>
+      </section>
+    </main>
   );
 }
