19ae7b370f83047807b65eb28394d89d47b6074a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-12 15:28:43 +0100

    03-jobify-app-nextjs-typescript: added components: LinksDropdown, Navbar, Sidebar, ThemeToggle

--- 03-jobify-app-nextjs-typescript/src/app/(dashboard)/layout.tsx
+++ 03-jobify-app-nextjs-typescript/src/app/(dashboard)/layout.tsx
@@ -1,14 +1,15 @@
+import { Navbar, Sidebar } from "@/components/navbar";
 import { type PropsWithChildren } from "react";
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
   return (
     <main className="grid lg:grid-cols-5">
       <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
-        Sidebar
+        <Sidebar />
       </div>
 
       <div className="lg:col-span-4">
-        Navbar
+        <Navbar />
         <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
       </div>
     </main>
---
+++ 03-jobify-app-nextjs-typescript/src/components/navbar/index.tsx
@@ -0,0 +1,3 @@
+export { default as Navbar } from "./Navbar";
+export { default as LinksDropdown } from "./LinksDropdown";
+export { default as Sidebar } from "./Sidebar";
