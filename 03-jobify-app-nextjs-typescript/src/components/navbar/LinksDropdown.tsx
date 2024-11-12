6d819915ee7436d8844cbc0c439c0b4601a6c022
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-12 21:39:38 +0100

    03-jobify-app-nextjs-typescript: DropdownList content

--- 03-jobify-app-nextjs-typescript/src/components/navbar/LinksDropdown.tsx
+++ 03-jobify-app-nextjs-typescript/src/components/navbar/LinksDropdown.tsx
@@ -1,3 +1,38 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { navLinks } from "@/utils/navLinks";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { AlignCenter } from "lucide-react";
+
 export default function LinksDropdown() {
-  return <div>LinkDropDown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignCenter />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="lg:hidden">
+        {navLinks.map((link) => (
+          <DropdownMenuItem key={link.label} asChild>
+            <Link
+              href={link.href}
+              className="flex items-center gap-x-4 cursor-pointer"
+            >
+              <>
+                {link.icon}
+                <span className="capitalize"> {link.label}</span>
+              </>
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
