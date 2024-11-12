6d9cc4bac4f879c05813465629402fbaba7c0015
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-12 16:19:19 +0100

    03-jobify-app-nextjs-typescript: Navbar content

--- 03-jobify-app-nextjs-typescript/src/components/navbar/Navbar.tsx
+++ 03-jobify-app-nextjs-typescript/src/components/navbar/Navbar.tsx
@@ -1,3 +1,24 @@
+import { UserButton } from "@clerk/nextjs";
+import { LinksDropdown, ThemeToggle } from "./index";
+import Image from "next/image";
+import logo from "./../../assets/logo-2.svg";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 flex items-center justify-between">
+      <div className="ml-2">
+        <LinksDropdown />
+      </div>
+
+      <Image
+        src={logo}
+        alt="logo image"
+        className="w-10 h-10 mx-auto lg:hidden"
+      />
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton />
+      </div>
+    </nav>
+  );
 }
--- 03-jobify-app-nextjs-typescript/src/components/navbar/index.tsx
+++ 03-jobify-app-nextjs-typescript/src/components/navbar/index.tsx
@@ -1,3 +1,4 @@
-export { default as Navbar } from "./Navbar";
 export { default as LinksDropdown } from "./LinksDropdown";
+export { default as Navbar } from "./Navbar";
 export { default as Sidebar } from "./Sidebar";
+export { default as ThemeToggle } from "./ThemeToggle";
