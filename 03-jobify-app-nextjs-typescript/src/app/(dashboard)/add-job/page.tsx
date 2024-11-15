5f3801591d079f92097a44ae02dc1862cf91b467
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-15 19:29:21 +0100

    03-jobify-app-nextjs-typescript: setup `react queries` and `shadcn/ui toast`

--- 03-jobify-app-nextjs-typescript/src/app/(dashboard)/add-job/page.tsx
+++ 03-jobify-app-nextjs-typescript/src/app/(dashboard)/add-job/page.tsx
@@ -1,9 +1,15 @@
 import CreateJobForm from "@/components/forms/CreateJobForm";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 export default function AddJobPage() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <CreateJobForm />
-    </>
+    </HydrationBoundary>
   );
 }
--- 03-jobify-app-nextjs-typescript/src/components/globals/Providers.tsx
+++ 03-jobify-app-nextjs-typescript/src/components/globals/Providers.tsx
@@ -1,12 +1,47 @@
 "use client";
 
 import { ThemeProvider } from "./ThemeProvider";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/ui/toaster";
 
 type TypeProps = {
   children: React.ReactNode;
 };
 
+/**
+ * **Implementation**: Using `useState`
+ *
+ * ```ts
+ * const [queryClient] = useState(() => new QueryClient({...}));
+ * ```
+ *
+ * **How it works**:
+ * - The `useState` hook ensures that the `QueryClient` instance is created only once per component lifecycle.
+ * - Even if the `Providers` component re-renders, the `QueryClient` will not be recreated, avoiding unnecessary resets of the client state and cache.
+ *
+ * **Advantages**:
+ * 1. **Avoids Recreation on Re-Renders**:
+ *    - This approach ensures that the `QueryClient` persists across renders, which is particularly important for preserving React Query’s cache and state.
+ * 2. **Recommended for Stable Instances**:
+ *    - Using `useState` is idiomatic for managing objects that you want to remain stable across renders.
+ */
+
 export function Providers({ children }: TypeProps) {
+  // Create a client
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000 * 5,
+          },
+        },
+      }),
+  );
   return (
     <>
       <ThemeProvider
@@ -15,7 +50,11 @@ export function Providers({ children }: TypeProps) {
         enableSystem
         disableTransitionOnChange
       >
-        {children}
+        <Toaster />
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
