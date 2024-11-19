5aaab70b59537ea94b9e747a9937c5efdaa7cb56
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-19 09:20:11 +0100

    03-jobify-app-nextjs-typescript: SearchForm Component - setup

--- 03-jobify-app-nextjs-typescript/src/components/jobs/JobSearchForm.tsx
+++ 03-jobify-app-nextjs-typescript/src/components/jobs/JobSearchForm.tsx
@@ -1,3 +1,54 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { EnumJobStatus } from "./utils/jobFormUtils";
+import { FormEvent } from "react";
+
 export default function SearchForm() {
-  return <div>SearchForm</div>;
+  // usePathname;
+  // useRouter;
+  // useSearchParams;
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e);
+    const formData = new FormData(e.currentTarget);
+    const search = formData.get("search") as string;
+    const jobStatus = formData.get("jobStatus") as string;
+    console.log(search, jobStatus);
+  };
+
+  return (
+    <form
+      className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
+      onSubmit={handleSubmit}
+    >
+      <Input type="text" placeholder="SearchJobs" name="search" />
+      <Select name="jobStatus">
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {[
+            "all",
+            ...Object.values(EnumJobStatus).map((jobStatus) => (
+              <SelectItem key={jobStatus} value={jobStatus}>
+                {jobStatus}
+              </SelectItem>
+            )),
+          ]}
+        </SelectContent>
+      </Select>
+      <Button type="submit">Search</Button>
+    </form>
+  );
 }
