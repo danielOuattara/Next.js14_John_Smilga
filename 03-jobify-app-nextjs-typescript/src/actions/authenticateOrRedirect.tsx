6f48b00047e3b5a246d2410030b82431b31481a2
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-27 07:49:43 +0100

    03-jobify-app-nextjs-typescript: cleaned up `authenticateOrRedirect( )`

--- 03-jobify-app-nextjs-typescript/src/actions/authenticateOrRedirect.tsx
+++ 03-jobify-app-nextjs-typescript/src/actions/authenticateOrRedirect.tsx
@@ -2,8 +2,8 @@
 import { auth } from "@clerk/nextjs/server";
 
 export default async function authenticateOrRedirect() {
-  const myAuth = await auth();
-  console.log(myAuth);
+  // const myAuth = await auth();
+  // console.log(myAuth);
   const { userId, redirectToSignIn } = await auth();
   if (!userId) {
     return redirectToSignIn();
